78eab394a5ae2a743da19a2b867e12fcb44e3591
StephenOo <hcqweb_front_end@163.com>
2019-10-18 19:41:30 +0800

    feature: update store/setting

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,45 +9,58 @@ const service = axios.create({
   timeout: 5000 // 请求超时时间
 })
 
-// request拦截器
+// request interceptor
 service.interceptors.request.use(config => {
-  // Do something before request is sent
+  // do something before request is sent
   if (store.getters.token) {
+    // let each request carry token
+    // ['X-Token] is a custom headers key
+    // please modify it according to the actual situation
     config.headers['X-Token'] = getToken() // 让每个请求携带token--['X-Token']为自定义key 请根据实际情况自行修改
   }
   return config
 }, error => {
   // Do something with request error
   console.log(error) // for debug
-  Promise.reject(error)
+  return Promise.reject(error)
 })
 
 // respone拦截器
 service.interceptors.response.use(
+  /**
+  * if you want to get http information such as headers or status
+  * Please return response => response
+  */
+
+  /**
+   * Determine the request status by custom code
+   * Here is just an example
+   * You can also judge the status by HTTP Status Code
+   */
   response => {
-    /**
-    * 下面的注释为通过response自定义code来标示请求状态，当code返回如下情况为权限有问题，登出并返回到登录页
-    * 如通过xmlhttprequest 状态码标识 逻辑可写在下面error中
-    */
     const res = response.data
+
+    // if the custom code is not 20000, it is judged as an error.
     if (res.code !== 20000) {
       Message.error(res.message)
-      // 50008:非法的token 50012:其他客户端登录了  50014:Token 过期了
+
+      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+        // to re-login
         Modal.confirm({
           title: '提示',
           content: '你已被登出，可以取消继续留在该页面，或者重新登录',
           okText: '重新登录',
           onOk: () => {
-            store.dispatch('FedLogOut').then(() => {
-              location.reload()// 为了重新实例化vue-router对象 避免bug
+            store.dispatch('user/resetToken').then(() => {
+              location.reload()
             })
           }
         })
       }
       return Promise.reject(new Error('error'))
     } else {
-      return response.data
+      return res
     }
   },
   error => {
